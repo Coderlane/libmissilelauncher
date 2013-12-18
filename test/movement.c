bc26ab64189182c6264e3ce21c67fd02f71bf416
Travis Lane <travis.lane.osx@gmail.com>
2013-12-17 18:41:26 -0800

    adding another test

---
+++ test/movement.c
@@ -0,0 +1,8 @@
+
+#include "movement.h"
+
+int main() {
+  TRACE("Initialized movement test\n");
+//  thunder_launcher launcher;
+  return 0;
+}
---
+++ test/movement.h
@@ -0,0 +1,5 @@
+
+
+#include "tl_control.h"
+#include <stdio.h>
+#include "../src/tlib_debug.h"
